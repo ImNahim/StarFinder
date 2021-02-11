b421ef8305b2fc23f7b87f58d6220b249ee40e2f
Nahim <slimanisphere@gmail.com>
2021-02-11 01:26:13 +0100

    cleaning imports, model

--- starwebsite/starapp/backend/main.py
+++ starwebsite/starapp/backend/main.py
@@ -1,19 +1,14 @@
 import ast
 import numpy as np
 import pandas as pd
-from keras.models import load_model
-import matplotlib.pyplot as plt
-from PIL import Image
+import os
+
 from .calculVectUser import calcul_vecteur
 
-import os
-path = os. getcwd()
+from keras.models import load_model
 
-
-# On load le model faceNet pré-etrainé ainsi que l'ensemble de ses poids
-#model = load_model("/res/model/model/facenet_keras.h5")
-#model.load_weights("/res/model/weights/facenet_keras_weights.h5")
-
+model = load_model(os.path.join(os.getcwd(), 'starapp/backend/resources/model/model','facenet_keras.h5'))
+model.load_weights(os.path.join(os.getcwd(), 'starapp/backend/resources/model/weights','facenet_keras_weights.h5'))
 
 # On calcule la similarité cosinus pour le vecteur représentant l'utilisateur et les vecteurs de la dataset
 def calculer_distance(vecteur_dataset, vecteur_utilisateur):
@@ -29,35 +24,7 @@ def calculer_distance(vecteur_dataset, vecteur_utilisateur):
 # Convertir les vecteurs en numpy array
 def convert_en_string(vecteur):
     vecteur = vecteur.replace('\n', '').replace('     ', ' ').replace('    ', ' ').replace('   ', ' ').replace('  ',' ').replace('[ ', '[').replace(' ]', ']').replace(' ', ', ')
-
-    # On return un numpy array
     return np.asarray(ast.literal_eval(vecteur)).astype('float32')
-
-
-def afficher_deux_images(img1, img2, nom_utilisateur, nom_celebrite='', fs=12):
-    f, axarr = plt.subplots(1, 2, figsize=(fs, fs))
-    axarr[0].imshow(img1)
-    axarr[0].title.set_text(nom_utilisateur)
-    axarr[1].imshow(img2)
-    axarr[1].title.set_text(nom_celebrite)
-    plt.setp(plt.gcf().get_axes(), xticks=[], yticks=[])
-    plt.show()
-
-
-# On affiche les  célébrités qui ressemblent le plus
-def afficher_celebrite(data_frame, path_image_utilisateur, nom_utilisateur, number=3):
-    for i in range(number):
-        instance = data_frame.iloc[i]
-        full_path = instance['file_path']
-        full_path = os.path.join(os.getcwd(), 'starapp','backend', full_path)
-        nom_celebrite = instance['name']
-        try:
-            img1 = Image.open(path_image_utilisateur)
-            img2 = Image.open(full_path).resize((512, 512))
-            afficher_deux_images(img1, img2, nom_utilisateur, nom_celebrite)
-
-        except FileNotFoundError:
-            print("L'image n'existe pas : " + full_path)
 
 def celebrity_path_and_name(data_frame):
     instance = data_frame.iloc[0]
@@ -66,17 +33,13 @@ def celebrity_path_and_name(data_frame):
     return path, nom_celebrite
 
 
-def main(path, vect, nom_utilisateur=''):
+def main(path):
+    vect_embedding = calcul_vecteur(path, model)
+    print(vect_embedding)
+
     path_df = os.path.join(os.getcwd(), 'starapp', 'backend','resources/vectorisation/imdb_metadata_v.csv')
     meta_data = pd.read_csv(path_df)
     meta_data["vecteur"] = meta_data["vecteur"].apply(lambda x: convert_en_string(x))
-    meta_data['distance'] = meta_data['vecteur'].apply(lambda x: calculer_distance(x, vect))
+    meta_data['distance'] = meta_data['vecteur'].apply(lambda x: calculer_distance(x, vect_embedding))
     meta_data = meta_data.sort_values(by=['distance'], ascending=True)
-    # afficher_celebrite(meta_data, path, nom_utilisateur)
-    #return meta_data
     return celebrity_path_and_name(meta_data)
-
-if __name__ == '__main__':
-    print('Entrez votre prénom')
-    nom = input()
-    main("starfinder/starwebsite/media/test.jpg",model =None, nom='')
